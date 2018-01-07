a3d01c15b9e6fff7624e059bd5c998b192c462f1
Joseph Batchik <josephbatchik@gmail.com>
2018-01-07 12:00:25 -0800

    working on the good suffix rule and trying to fix some tests

--- src/search/search.rs
+++ src/search/search.rs
@@ -23,6 +23,96 @@ macro_rules! init_array(
     )
 );
 
+fn match_length(pattern: &Vec<char>, mut i1: usize, mut i2: usize) -> usize {
+    if i1 == i2 {
+        return pattern.len() - i1;
+    } else {
+        let mut count: usize = 0;
+        while i1 < pattern.len() && i2 < pattern.len() && pattern[i1] == pattern[i2] {
+            count += 1;
+            i1 += 1;
+            i2 += 1;            
+        }
+        return count;
+    }
+}
+
+fn preprocess(pattern: &str) -> Vec<usize> {
+    if pattern.len() == 0 {
+        return vec![];
+    } else if pattern.len() == 1 {
+        return vec![1];
+    } else {
+        let chars: Vec<char> = pattern.chars().collect();
+        let mut result = vec![0; pattern.len()];
+        result[0] = pattern.len();
+        result[1] = match_length(&chars, 0, 1);
+
+        for i in 2..(1 + result[1]) {
+            result[i] = result[1] - i + 1;
+        }
+
+        let mut left = 0;
+        let mut right = 0;
+
+        for i in (2 + result[1])..(pattern.len()) {
+            if i <= right {
+                let k = i - left;
+                let b = result[k];
+                let a = right - i + 1;
+                if b < a {
+                    result[i] = b;
+                } else {
+                    result[i] = match_length(&chars, a, right + 1);
+                    left = i;
+                    right = i + result[i] - 1;
+                }
+            } else {
+                result[i] = match_length(&chars, 0, i);
+                if result[i] > 0 {
+                    left = i;
+                    right = i + result[i] - 1;
+                }
+            }
+        }
+        return result;
+    }
+}
+
+fn get_good_suffix(pattern: &str) -> Vec<i32> {
+    let mut result: Vec<i32> = vec![-1; pattern.len()];
+    let mut preprocess = preprocess(pattern);
+    preprocess.reverse();
+
+    for i in 0..(pattern.len() - 1) {
+        let j = pattern.len() - preprocess[i];
+        if j != preprocess.len() {
+            result[j] = i as i32;
+        }
+    }
+
+    return result;
+}
+
+fn get_full_shift(pattern: &str) -> Vec<usize> {
+    let mut result = vec![0 ; pattern.len()];
+    let mut z = preprocess(pattern);
+    z.reverse();
+    let mut longest: usize = 0;
+
+    for (index, value) in z.iter().enumerate() {
+        if *value == index + 1 {
+            if *value > longest {
+                longest = *value;
+            }
+        }
+        let i = result.len() - index - 1;
+        result[i] = longest; 
+    }
+
+    return result;    
+}
+
 fn get_bad_character(pattern: &str) -> [Vec<i32>; ALPHABET_SIZE] {
     let mut result: [Vec<i32>; ALPHABET_SIZE] =
         init_array!(Vec<i32>, ALPHABET_SIZE, Vec::with_capacity(pattern.len()));
@@ -137,6 +227,8 @@ fn search(pattern: &str, contents: &Vec<char>) -> Vec<usize> {
         return results;
     }
     let bad_char_table = get_bad_character(pattern);
+    let good_suffix = get_good_suffix(pattern);
+    let full_shift = get_full_shift(pattern);
 
     // alignment of the end of the pattern relative to the search
     let mut k: usize = pattern.len() - 1; 
@@ -158,21 +250,38 @@ fn search(pattern: &str, contents: &Vec<char>) -> Vec<usize> {
 
         if valid { // match found
             let i = k + 1 - pattern.len();
+            println!("match found at {}", i);
             results.push(i);
             k += 1;
-        } else { // no match
+        } else { // no match, calculate shift distance
             let bad_char = bad_char_table[contents[c_index] as usize][p_index];
-            let char_shift;
-            if bad_char == -1 {
-                char_shift = p_index + 1;
+            let mut char_shift = p_index as i32 - bad_char;
+
+            let suffix_shift;
+            if p_index + 1 == pattern.len() {
+                suffix_shift = 1;
+            } else if good_suffix[p_index + 1] == -1 {
+                // matched suffix does not appear anywhere in the input pattern
+                suffix_shift = (pattern.len() - full_shift[p_index + 1]) as i32;
             } else {
-                char_shift = p_index - bad_char as usize;
+                // matched suffix does appear in the input pattern
+                suffix_shift = pattern.len() as i32 - good_suffix[p_index + 1];
             }
 
-            if char_shift >= p_index + 1 {
+            let shift;
+            if char_shift > suffix_shift {
+                println!("bad character shift");
+                shift = char_shift;
+            } else {
+                println!("good suffix shift");
+                shift = suffix_shift;
+            }
+            
+            if shift >= p_index as i32 + 1 {
                 prev_k = k as i32;
             }
-            k+= char_shift;
+            println!("shift is {} to {}", shift, k);
+            k += shift as usize;
         }
     }
     return results;
@@ -187,6 +296,13 @@ mod tests {
         let results = search_string("pattern", "a pattern to find".to_string());
         assert_eq!(1, results.len(), "only one result should be returned");
         assert_eq!(2, results[0], "correct index");
+    }
+
+    #[test]
+    fn pattern_at_end() {
+        let results = search_string("test", "find test".to_string());
+        assert_eq!(1, results.len());
+        assert_eq!(5, results[0]);
     }
 
     #[test]
@@ -245,5 +361,68 @@ mod tests {
     fn all_matches() {
         let results = search_string("j", "jjjjjjjjjj".to_string());
         assert_eq!(10, results.len(), "all matches");
+    }
+
+    #[test]
+    fn match_length_equal() {
+        let chars = "this is a test of this".chars().collect();
+        let result = super::match_length(&chars, 0, 0);
+        assert_eq!(chars.len(), result, "match should be entire string");
+    }
+
+    #[test]
+    fn match_length_different() {
+        let chars = "this is a this of this".chars().collect();
+        let result = super::match_length(&chars, 0, 10);
+        assert_eq!(5, result, "match all of `this `");
+    }
+
+    #[test]
+    fn match_length_no_match() {
+        let chars = "this is a -es- of no-hing".chars().collect();
+        let result = super::match_length(&chars, 0, 10);
+        assert_eq!(0, result, "no match");
+    }
+
+    #[test]
+    fn small_preprocess() {
+        let result = super::preprocess("a");
+        assert_eq!(1, result.len(), "correct size");
+        assert_eq!(1, result[0], "correct substring size");
+    }
+
+    #[test]
+    fn empty_preprocess() {
+        assert_eq!(0, super::preprocess("").len(), "empty result");
+    }
+
+    #[test]
+    fn simple_preprocess() {
+        let input = "hanhan";
+        let result = super::preprocess(input);        
+        assert_eq!(input.len(), result[0]);
+        assert_eq!(0, result[1]);
+        assert_eq!(3, result[3]);
+    }
+
+    #[test]
+    fn full_shift_simple() {
+        let input = "hanhan";
+        let result = super::get_full_shift(input);
+
+        assert_eq!(input.len(), result.len());
+        assert_eq!(3, result[3]);
+        assert_eq!(3, result[2]);
+    }
+
+    #[test]
+    fn full_shift_no_result() {
+        let input = "abcdefghijklm";
+        let result = super::get_full_shift(input);
+        assert_eq!(13, result[0], "the full body is a prefix and suffix");
+        
+        for i in 1..13 {
+            assert_eq!(0, result[i], "should be no results for {}", i);
+        }
     }
 }
